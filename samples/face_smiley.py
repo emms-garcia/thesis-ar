fb775cfbf93b3000dec2e67de054beb9ed72f548
root <root@emmanuel-lap.(none)>
2014-01-29 22:11:56 -0600

    Dibujar lineas con centros de circulos detectados

--- samples/face_smiley.py
+++ samples/face_smiley.py
@@ -7,11 +7,14 @@ Small edit. Show smiley on detected face
 """
  
 import cv2
- 
+import math
+import cv
+import numpy as np 
+
 TRAINSET = "data/lbpcascade_frontalface.xml"
 DOWNSCALE = 4
 smiley = cv2.imread("data/smiley.png", -1)
- 
+dist_umb = 100
 webcam = cv2.VideoCapture(0)
 cv2.namedWindow("preview")
 classifier = cv2.CascadeClassifier(TRAINSET)
@@ -21,9 +24,23 @@ if webcam.isOpened(): # try to get the first frame
     rval, frame = webcam.read()
 else:
     rval = False
- 
+lines = []
 while rval:
-  # detect faces and draw bounding boxes
+  rval, frame = webcam.read()
+  cv2.flip(frame,1,frame)
+
+  # get next frame
+  gray = cv2.cvtColor(frame, cv.CV_RGB2GRAY)
+  blur = cv2.GaussianBlur(gray, (0,0), 2)
+  result =  cv2.HoughCircles(blur, cv2.cv.CV_HOUGH_GRADIENT, 2, 10, np.array([]), 40, 80, 5, 100)
+  if result is not None:
+   #Ignorar repetidos
+    for c1 in result[0]:
+      cv2.circle(frame, (c1[0],c1[1]), c1[2], (0,255,0),2)
+      for c2 in result[0]:
+        if math.sqrt(math.pow(c2[1] - c1[1], 2) + math.pow(c2[0] - c1[0], 2)) > dist_umb:
+          lines.append([c1[0],c1[1],c2[0],c2[1]])
+
   minisize = (frame.shape[1]/DOWNSCALE,frame.shape[0]/DOWNSCALE)
   miniframe = cv2.resize(frame, minisize)
   faces = classifier.detectMultiScale(miniframe)
@@ -38,12 +55,13 @@ while rval:
                  frame[y:y+smiley.shape[0], x:x+smiley.shape[1], c] * \
                  (1.0 - smiley[:,:,3]/255.0)
 
-  cv2.putText(frame, "Press ESC to close.", (5, 25),
-              cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
-  cv2.imshow("preview", frame)
+  for line in lines:
+    cv2.line(frame, (line[0],line[1]), (line[2],line[3]), (0,0,255), 3)
 
-  # get next frame
-  rval, frame = webcam.read()
+  cv2.imwrite("%s.png"%len(lines),frame)
+  cv2.putText(frame, "Press ESC to close.", (5, 25),
+            cv2.FONT_HERSHEY_SIMPLEX, 1.0, (255,255,255))
+  cv2.imshow("preview", frame)
   key = cv2.waitKey(20)
   if key in [27, ord('Q'), ord('q')]: # exit on ESC
     break
