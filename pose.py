72654fe8b0694bf7ed12a722ead74c25dfcbf7ad
Alex <aleksgs10@gmail.com>
2014-02-27 16:28:02 -0600

    Clase para calibrar camara

--- pose.py
+++ pose.py
@@ -4,6 +4,35 @@ import numpy as np
 import cv2
 import glob
 
+class Camera:
+ 
+  def __init__(self):
+    self.matrix = None
+    self.objPoints = np.zeros((6*7, 3), np.float32)
+    self.objPoints[:,:2] = np.mgrid[0:7, 0:6].T.reshape(-1, 2)
+    self.distCoeffs = None
+    self.criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    self.objectpoints = []
+    self.imagepoints = []
+    self.calibratedMatrix = None
+    
+  def calibrateWithImages(self, images):
+    for fname in images:
+      img = cv2.imread(fname)
+      gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+      ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+      if ret:
+        self.objectpoints.append(self.objPoints)
+        cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), self.criteria)
+        self.imagepoints.append(corners)
+    ret, self.matrix, self.distCoeffs, _, _ = cv2.calibrateCamera(self.objectpoints, self.imagepoints, gray.shape[::-1], None, None)
+    h, w = img.shape[:2]
+    self.calibratedMatrix, roi = cv2.getOptimalNewCameraMatrix(self.matrix, self.distCoeffs, (w, h), 1, (w, h))
+    return self.matrix
+  
+  def calibrateWithCamera(self, camera):
+    return
+
 def draw(img, corners, imgpts):
   corner = tuple(corners[0].ravel())
   cv2.line(img, corner, tuple(imgpts[0].ravel()), (0,255,0), 5)
@@ -11,50 +40,23 @@ def draw(img, corners, imgpts):
   cv2.line(img, corner, tuple(imgpts[2].ravel()), (255,0,0), 5)
   return img
 
-# termination criteria
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-
-# Arrays to store object points and image points from all the images.
-objpoints = [] # 3d point in real world space
-imgpoints = [] # 2d points in image plane.
-
-images = glob.glob('samples/*.jpg')
+images = glob.glob('*.jpg')
+camera = Camera()
+camera.calibrateWithImages(images)
+axis = np.float32([[3, 0, 0], [0, 3, 0], [0, 0, -3]]).reshape(-1, 3)
 
 for fname in images:
   img = cv2.imread(fname)
-  gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-  ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
-  if ret == True:
-    objpoints.append(objp)
-    corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-    imgpoints.append(corners)
-    
-ret, mtx, dist, _, _ = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-objp = np.zeros((6*7,3), np.float32)
-objp[:,:2] = np.mgrid[0:7,0:6].T.reshape(-1,2)
-
-axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
-
-for fname in glob.glob('samples/left*.jpg'):
-  img = cv2.imread(fname)
   h,  w = img.shape[:2]
-  newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-  gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-  img = cv2.undistort(img, mtx, dist, None, newcameramtx)
-  cv2.imwrite("samples/"+fname[:6]+'_undistorted.png', img)
-  ret, corners = cv2.findChessboardCorners(gray, (7,6),None)
-
-  if ret == True:
-    cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-    rvecs, tvecs, inliers = cv2.solvePnPRansac(objp, corners, newcameramtx, dist)
-    imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, newcameramtx, dist)
-    img = draw(img,corners,imgpts)
-    cv2.imwrite("samples/"+fname[:6]+'.png', img)
+  gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+  img = cv2.undistort(img, camera.matrix, camera.distCoeffs, None, camera.calibratedMatrix)
+  cv2.imwrite(fname[:6]+ '_undistorted.png', img)
+  ret, corners = cv2.findChessboardCorners(gray, (7, 6), None)
+  if ret:
+    cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), camera.criteria)
+    rvecs, tvecs, inliers = cv2.solvePnPRansac(camera.objPoints, corners, camera.calibratedMatrix, camera.distCoeffs)
+    imgpts, jac = cv2.projectPoints(axis, rvecs, tvecs, camera.calibratedMatrix, camera.distCoeffs)
+    img = draw(img, corners, imgpts)
+    cv2.imwrite(fname[:6]+ '.png', img)
 
 cv2.destroyAllWindows()
