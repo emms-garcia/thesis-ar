d61791fc8d5771dc01fab18a336b531c3c31d42b
Alex <aleksgs10@gmail.com>
2014-04-01 07:34:08 -0600

    Reusar modelos de marcadores no perdidos

--- main.py
+++ main.py
@@ -3,20 +3,23 @@
 # Librerias usadas
 import cv2
 import qr
-import numpy as np
 import three
 import pygame
 import os
 import MeshViewer
-import random
 
 # Utils
 SAMPLES_DIR = os.path.join("samples", "models")
 VIDEO_DIR = os.path.join("samples", "videos")
 models = {
   "1": os.path.join(SAMPLES_DIR, "1.obj"),
-  "2": os.path.join(SAMPLES_DIR, "2.obj")
+  "2": os.path.join(SAMPLES_DIR, "2.obj"),
+  "3": os.path.join(SAMPLES_DIR, "3.obj"),
+  "4": os.path.join(SAMPLES_DIR, "4.obj"),
+  "5": os.path.join(SAMPLES_DIR, "5.obj"),
+  "6": os.path.join(SAMPLES_DIR, "6.obj")
 }
+found_markers = {}
 
 # OpenCV
 FRAME_SIZE = (400, 300)
@@ -35,7 +38,6 @@ def main():
   clock = pygame.time.Clock()
   screen = pygame.display.set_mode(FRAME_SIZE)
   env3d = MeshViewer.Env3D(screen, [FRAME_SIZE[0], FRAME_SIZE[1]])
-  
   # OpenCV
   video_capture = cv2.VideoCapture(os.path.join(VIDEO_DIR, "test_qr.mp4"))
   ret = True
@@ -59,20 +61,24 @@ def main():
           cv2.rectangle(frame, (bb.min.x, bb.min.y), (bb.max.x, bb.max.y), (255, 0, 0))
           #cv2.putText(frame, data.data, (bb.center.x, bb.center.y), cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
           try:
-            objects.append(MeshViewer.Object(models[str(data.data)]))
-            mesh_center = objects[-1].getCenter()
+            if data.data in found_markers:
+              object = found_markers[data.data]
+            else:
+              object = MeshViewer.Object(models[str(data.data)])
+              found_markers[str(data.data)] = object
+            mesh_center = object.getCenter()
             # Normalizar, se dibuja desde el centro
             center = three.Vector2((mesh_center.x + FRAME_SIZE[0]/2.0, mesh_center.y + FRAME_SIZE[1]/2.0))
             offset = bb.center.sub(center)
-            objects[-1].translate(MeshViewer.Point3D(offset.x, offset.y, 0))
+            object.translate(MeshViewer.Point3D(offset.x, offset.y, 0))
           except Exception as e:
             print "No model found for marker: " + data.data
       # Pygame
       pygame_image = pygame.image.frombuffer(frame.tostring(), frame.shape[1::-1], "RGB")
       screen.blit(pygame_image, (0, 0))
-      if len(objects) > 0:
-        for object in objects:
-          object.display(env3d)
+      if len(found_markers) > 0:
+        for key in found_markers:
+          found_markers[key].display(env3d)
       pygame.display.update()
       clock.tick(fps)
     # No mas frames
